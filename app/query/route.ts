ebd7a219df084d8d50deb6e37c66879f15b3e344
DENTAGEST\rayan.boiteau <rayan.boiteau@dentagest.ch>
2025-02-03 15:19:27 +0100

    end of chap 6

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,26 +1,27 @@
-// import postgres from 'postgres';
+import postgres from 'postgres';
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+// Configuration de la connexion à la base de données
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
-// async function listInvoices() {
-// 	const data = await sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
+// Fonction pour exécuter la requête SQL
+async function listInvoices() {
+  const data = await sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
+  `;
 
-// 	return data;
-// }
+  return data;
+}
 
+// Fonction GET pour retourner les données
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
+  try {
+    // Retourne les données de la requête SQL
+    return Response.json(await listInvoices());
+  } catch (error) {
+    // Gestion des erreurs
+    return Response.json({ error }, { status: 500 });
+  }
 }
